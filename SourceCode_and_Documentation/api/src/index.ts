a2ce8c7da3d23051135bcbf0fc79f09332ef47c1
The TT Hacker <thetthacker@localhost.localdomain>
2020-03-08 07:46:37 +1100

    Added some basic api endpoints

--- SourceCode_and_Documentation/api/src/index.ts
+++ SourceCode_and_Documentation/api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import {
   getClassses
 } from "./common/database";
@@ -8,28 +9,77 @@ const PORT = 8080;
 
 // Testing scraper
 //var classes = await getAllClasses();
-getUnswTimetable().then((classes) => console.log(classes));
+//getUnswTimetable().then((classes) => console.log(classes));
 
 const app = express();
 
-// Root
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send(JSON.stringify({
   "name": "Connect on Campus API",
   "version": "1.0.0"
 })));
 
-// Classes
-app.get('/classes', async (req, res) => {
+/*
+  Timtable endpoints
+
+  Get all faculties
+  Get all classes
+*/
+
+// Get all classes
+app.get('/events/classes', async (req, res) => {
   const classes = await getClassses();
   res.send(JSON.stringify(classes));
 });
 
-app.get('/timetable', async (req, res) => {
+/*
+  Event end points
+
+  Get latest events
+  Get event by id
+  Post - Going to event
+  Puts - Undo going to event
+*/
+
+app.get('/events', async (req, res) => {
   const classes = await getClassses();
   res.send(JSON.stringify(classes));
 });
 
-// Run Server
+/*
+  User end points
+
+  Post Login
+  Post Logout
+  Post Register
+  Puts Updates
+*/
+
+app.post('/user/login', async (req, res) => {
+  const classes = await getClassses();
+  res.send(JSON.stringify(classes));
+});
+
+/*
+  Match endpoints
+
+  
+*/
+
+app.post('/match', async (req, res) => {
+  const classes = await getClassses();
+  res.send(JSON.stringify(classes));
+});
+
+/*
+  Chat endpoints
+*/
+
+/*
+
+*/
+
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
 });
