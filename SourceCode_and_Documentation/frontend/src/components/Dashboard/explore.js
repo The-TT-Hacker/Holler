33f50e4f891d9edc710460e408e33c4568ce8159
Andrew Han <ajh5273@gmail.com>
2020-04-10 14:46:48 +1000

    Finished the UI for the Explore page

--- SourceCode_and_Documentation/frontend/src/components/Dashboard/explore.js
+++ SourceCode_and_Documentation/frontend/src/components/Dashboard/explore.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-
-import { ToggleButtonGroup, ToggleButton, Carousel, Card, Form, FormControl, Button } from 'react-bootstrap'
+import { ToggleButtonGroup, ToggleButton, Carousel, Card, Form, FormControl, Button, Modal } from 'react-bootstrap'
 import EventImage from '../../icons/event-image.svg'
 import EventImage2 from '../../icons/event-image-2.svg'
 import ChatBubble from '../../icons/chat.svg'
 import '../../styles/events.css'
 import '../../styles/explore.css'
+import { DateRangePicker, Calendar } from 'react-date-range'
+import 'react-date-range/dist/styles.css' // main css file
+import 'react-date-range/dist/theme/default.css' // theme css file
+
 
 const Explore = () => {
   const [index, setIndex] = useState(0)
@@ -15,7 +18,7 @@ const Explore = () => {
 
   const [value, setValue] = useState([])
   const handleChange = (val) => setValue(val)
-  
+
   const [active, setActive] = useState(false)
   const displaySearchBox = () => {
     if (active) {
@@ -28,11 +31,22 @@ const Explore = () => {
     setActive(!active)
   }
 
+  const [tagModalShow, setTagModalShow] = useState(false);
+  const [dateModalShow, setDateModalShow] = useState(false);
+
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  }
+  const handleDate = (ranges) => {
+    selectionRange.startDate = ranges.selection.startDate;
+    selectionRange.endDate = ranges.selection.endDate;
+  }
+
   return (
     <div>
       <br></br>
-
-
       <div className="container-fluid" style={{ width: '72%' }}>
         <div className="row">
           <div className="col-7 d-flex align-items-start justify-content-start">
@@ -46,8 +60,95 @@ const Explore = () => {
           </div>
         </div>
         <div className="row d-flex justify-content-start">
-          <Button className="btn-filters">Tags</Button>
-          <Button className="btn-filters">Date</Button>
+          <Button onClick={() => setTagModalShow(true)} className="btn-filters">Tags</Button>
+          <Modal show={tagModalShow}
+            onHide={() => setTagModalShow(false)}
+            aria-labelledby="tag-modal-title">
+            <Modal.Header>
+              <Modal.Title id="tag-modal-title">Filter By Tags</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+
+              <div className="col d-flex flex-column align-items-center" style={{ padding: 0 }}>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={0}> Photography </ToggleButton>
+                    <ToggleButton className="btn-tags" value={1}> Family </ToggleButton>
+                    <ToggleButton className="btn-tags" value={2}> Concerts </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={3}> Running </ToggleButton>
+                    <ToggleButton className="btn-tags" value={4}> Gaming </ToggleButton>
+                    <ToggleButton className="btn-tags" value={5}> Science </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={6}> Cinema </ToggleButton>
+                    <ToggleButton className="btn-tags" value={7}> Dance </ToggleButton>
+                    <ToggleButton className="btn-tags" value={8}> Music </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={9}> Study </ToggleButton>
+                    <ToggleButton className="btn-tags" value={10}> Hiking </ToggleButton>
+                    <ToggleButton className="btn-tags" value={11}> Beach </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={12}> Gambling </ToggleButton>
+                    <ToggleButton className="btn-tags" value={13}> Partying </ToggleButton>
+                    <ToggleButton className="btn-tags" value={14}> Trivia </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div className="row">
+                  <ToggleButtonGroup className="" type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton className="btn-tags" value={15}> Karaoke </ToggleButton>
+                    <ToggleButton className="btn-tags" value={16}> Squash </ToggleButton>
+                    <ToggleButton className="btn-tags" value={17}> Food </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+              </div>
+
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="btn-modal-done btn-lg" variant="primary" onClick={() => setTagModalShow(false)}>
+                Done
+              </Button>
+
+            </Modal.Footer>
+          </Modal>
+
+          <Button onClick={() => setDateModalShow(true)} className="btn-filters">Date</Button>
+          <Modal show={dateModalShow}
+            onHide={() => setDateModalShow(false)}
+            aria-labelledby="date-modal-title">
+            <Modal.Header>
+              <Modal.Title id="date-modal-title">Select Dates</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="col d-flex flex-column align-items-center" style={{ padding: 0 }}>
+                <DateRangePicker
+                  ranges={[selectionRange]}
+                  onChange={handleDate}
+                  minDate={new Date()}
+                  rangeColors={['#FEA31F','#FEA31F','#FEA31F','#FEA31F']}
+                />
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="btn-modal-done btn-lg" variant="primary" onClick={() => setDateModalShow(false)}>
+                Done
+              </Button>
+
+            </Modal.Footer>
+          </Modal>
+
+
         </div>
       </div>
 
