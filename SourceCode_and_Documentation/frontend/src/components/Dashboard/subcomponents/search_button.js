fc00ce16522fd9708039f4c33e5154de29d7c453
Andrew Han <ajh5273@gmail.com>
2020-04-12 15:45:01 +1000

    Reverting search bar to collapsing form to allow responsiveness in Explore page

--- SourceCode_and_Documentation/frontend/src/components/Dashboard/subcomponents/search_button.js
+++ SourceCode_and_Documentation/frontend/src/components/Dashboard/subcomponents/search_button.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react'
-import { Card, Form, FormControl, Button, Accordion} from 'react-bootstrap'
-import { useAccordionToggle } from 'react-bootstrap/AccordionToggle'
+import { Form, FormControl, ToggleButton, ToggleButtonGroup, Collapse} from 'react-bootstrap'
 
 
-function SearchBar ({children,eventKey}) {
-    const [active, setActive] = useState(false)
-    const displaySearchBox =  useAccordionToggle(eventKey, () =>{
-
-        if (active) {
-            document.getElementById("btn-explore-search").classList.remove("active")
-        } else {
-            document.getElementById("btn-explore-search").classList.add("active")
-        }
-        setActive(!active)
-    })
-
-    return (
-        <Button id="btn-explore-search" onClick={displaySearchBox}> </Button>
-    )
-}
 
 const SearchButton = () => {
+    const [value, setValue] = useState([])
+    const handleChange = (val) => setValue(val)
+
+    const onFormSubmit = e => {
+        e.preventDefault();
+        console.log("searching");
+        // search stuff
+    }
+
+    const [active, setActive] = useState(false)
+    const displaySearchBox = () => {
+
+        if (active) {
+            document.getElementById("search-box-input").style.display('none');
+            document.getElementById("btn-explore-search").classList.remove("active");
+        } else {
+            document.getElementById("search-box-input").style.display('block');
+            document.getElementById("btn-explore-search").classList.add("active");
+        }
+        setActive(!active);
+    }
+
+
     return (
+
         <div>
-            <Accordion defaultActiveKey="0">
-                <Card>
-                    <Card.Header>
-                        <SearchBar eventKey="0"></SearchBar>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>
-                            <Form className="search-box-form" id="search-box-input" >
-                                <FormControl type="text" placeholder="Search" />
-                            </Form>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
+            <div className="col-5 d-flex justify-content-end">
+                <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+                    <ToggleButton
+                        onClick={() => {setOpen(!open);
+                                        displaySearchBox;}}
+                        aria-controls="collapse-search"
+                        aria-expanded={open}
+                        className="btn-explore-search" value={"search-button"}>
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </div>
+            <Collapse in={open}>
+                <div id="collapse-search" className="row d-flex align-items-center justify-content-end">
+                    <Form className="search-box-form" id="search-box-input" onSubmit={onFormSubmit} inline>
+                        <FormControl type="text" placeholder="Search" />
+                    </Form>
+                </div>
+            </Collapse>
         </div>
     )
 }
