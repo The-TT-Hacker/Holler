a11c0ff97248e9cafb54d0f3870d61a8fd48adb4
Andrew Han <ajh5273@gmail.com>
2020-04-16 18:08:13 +1000

    Added starter code for connecting to the backend

--- SourceCode_and_Documentation/frontend/src/components/Dashboard/profile.js
+++ SourceCode_and_Documentation/frontend/src/components/Dashboard/profile.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-import { Card, Button} from 'react-bootstrap'
-
+import { Card, Button } from 'react-bootstrap'
+//import { withAuthentication } from '../Session'
+import { URL } from '../../constants/roles'
 import { PageTitle, Avatar, Badges, Chart } from './subcomponents'
 import { updateScrollability } from '../../constants';
 
@@ -11,6 +13,57 @@ import '../../styles/explore.css'
 const Profile = (props) => {
 
   updateScrollability(true)
+
+  // global error variable
+  const [error, setError] = useState("")
+  const g_token = localStorage.getItem('token') // global token
+  console.log(error)
+
+  // get information from backend
+  useEffect(() => {
+
+    // Flag to use for cleanup
+    const source = axios.CancelToken.source()
+
+    // auth token - in useEffect to supress depdendency warnings
+    const token = localStorage.getItem('token')
+    console.log(token);
+    // Request Function
+    const fetchUserData = async () => {
+
+      await axios({
+        url: URL + "/user",
+        method: "GET",
+        cancelToken: source.token,
+        headers: {
+          'Authorization': `${token}`
+        },
+      })
+        .then(res => {
+
+          console.log(res)
+
+        })
+        .catch(err => {
+
+          if (axios.isCancel(err)) {
+            setError(err)
+          } else {
+            setError(err)
+          }
+
+        })
+    }
+
+    // Make the request
+    fetchUserData()
+
+    // Cancel other requests
+    return () => {
+      source.cancel()
+    }
+
+  }, [])
 
   return (
     <div className="container-fluid d-flex flex-column align-items-center">
@@ -32,31 +85,31 @@ const Profile = (props) => {
           </div>
         </div>
 
-        <Badges/>
-        
-        <Chart/>
-      
+        <Badges />
+
+        <Chart />
+
         <Card className="profile-card">
           <Card.Header>
-              <span className="txt-bold txt-md"> Personal Information </span>
-            </Card.Header>
-            <Card.Body>
-              <div className="clearfix">
-                <div className="float-left txt-poppins"> Name </div>
-                <div className="float-right txt-poppins"> Bob Cheers </div>
-              </div>
-              <div className="clearfix">
-                <div className="float-left txt-poppins"> Birthday </div>
-                <div className="float-right txt-poppins"> 12/04/1995 </div>
-              </div>
-              <div className="clearfix">
-                <div className="float-left txt-poppins"> Email </div>
-                <div className="float-right txt-poppins"> b.cheers@student.unsw.edu.au </div>
-              </div>
-              <div className="clearfix">
+            <span className="txt-bold txt-md"> Personal Information </span>
+          </Card.Header>
+          <Card.Body>
+            <div className="clearfix">
+              <div className="float-left txt-poppins"> Name </div>
+              <div className="float-right txt-poppins"> Bob Cheers </div>
+            </div>
+            <div className="clearfix">
+              <div className="float-left txt-poppins"> Birthday </div>
+              <div className="float-right txt-poppins"> 12/04/1995 </div>
+            </div>
+            <div className="clearfix">
+              <div className="float-left txt-poppins"> Email </div>
+              <div className="float-right txt-poppins"> b.cheers@student.unsw.edu.au </div>
+            </div>
+            {/* <div className="clearfix">
                 <span type="button" className="txt-gradient txt-bold float-right"> Edit </span>
-              </div>
-            </Card.Body>
+              </div> */}
+          </Card.Body>
         </Card>
 
         <Card className="profile-card">
@@ -72,9 +125,9 @@ const Profile = (props) => {
               <div className="float-left txt-poppins"> School </div>
               <div className="float-right txt-poppins"> Engineering/Arts </div>
             </div>
-            <div className="clearfix">
+            {/* <div className="clearfix">
               <span type="button" className="txt-gradient txt-bold float-right"> Edit </span>
-            </div>
+            </div> */}
           </Card.Body>
         </Card>
 
