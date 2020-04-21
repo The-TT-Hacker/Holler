176ef6aad50d60416c386c98179643102638f0c1
The-TT-Hacker <timthacker97@gmail.com>
2020-04-21 22:16:58 +1000

    events scraper working for coordinates

--- SourceCode_and_Documentation/api/src/models/event.ts
+++ SourceCode_and_Documentation/api/src/models/event.ts
@@ -1,3 +1,7 @@
+
+/**
+ * Firebase object for EventInterest
+ */
 export interface Event {
   url: string;
   image_url: string;
@@ -12,6 +16,9 @@ export interface Event {
   categories: string[];
 }
 
+/**
+ * Object used for adding an event
+ */
 export interface AddEventRequest {
   id: string;
   url: string;
@@ -27,6 +34,27 @@ export interface AddEventRequest {
   categories: string[];
 }
 
+/**
+ * Object that is returned when an event is requested
+ */
+export interface GetEventResponse {
+  id: string;
+  url: string;
+  image_url: string;
+  title: string;
+  time_start: Date;
+  time_finish: Date;
+  description: string;
+  location: string;
+  latitude?: number;
+  longitude?: number;
+  hosts: string[];
+  categories: string[];
+}
+
+/**
+ * Firebase object for EventInterest
+ */
 export interface EventInterest {
   uid: string;
   eventId: string;
--- SourceCode_and_Documentation/api/src/services/eventService.ts
+++ SourceCode_and_Documentation/api/src/services/eventService.ts
@@ -3,7 +3,7 @@ import { db } from "./firebaseService";
 import * as googleMapsService from "./googleMapsService";
 
 // Import models
-import { Event, EventInterest, AddEventRequest } from "../models/event";
+import { Event, EventInterest, AddEventRequest, GetEventResponse } from "../models/event";
 
 declare global {
   interface Date {
@@ -49,7 +49,9 @@ export async function getEvents(searchText: string, tags: string, startDate: str
     const snapshot = await query.get()
     var events = <Event[]> snapshot.docs.map(doc => {
       const docData = doc.data();
-      return {
+
+      const eventResponse: GetEventResponse = {
+        id: doc.id,
         url: docData.url,
         title: docData.title,
         time_start: docData.time_start.toDate(),
@@ -60,6 +62,8 @@ export async function getEvents(searchText: string, tags: string, startDate: str
         categories: docData.categories,
         image_url: docData.image_url
       }
+
+      return eventResponse;
     });
 
     // Filter events
@@ -130,7 +134,6 @@ export async function getEvent(id: string): Promise<Event> {
     }
 
   } catch (e) {
-    console.log(e);
     throw e;
   }
 
@@ -183,27 +186,44 @@ export async function setEvent(id: string, event: Event): Promise<void> {
 
     const currentEvent = await getEvent(id);
 
-    // Check if location has been updated
-    if (event.location === currentEvent.location)
-      newOrUpdatedLocation = false;
-    else
-      newOrUpdatedLocation = true;
-0
+    // Event location has not been updated
+    if (event.location === currentEvent.location) newOrUpdatedLocation = true;
+    
+    // Event location updated
+    else newOrUpdatedLocation = false;
+
+  // New event
   } catch {
 
-    // New event
     newOrUpdatedLocation = true;
   
   }
 
   // Get coordinates if event is new or location is updated
   if (newOrUpdatedLocation) {
-    googleMapsService.getCoordinates(event.location);
+
+    try {
+      
+      //const coordinates = { latitude: 0, longitude: 0 };
+      const coordinates = await googleMapsService.getCoordinates(event.location);
+      console.log(coordinates);
+      
+      event.latitude = coordinates.latitude;
+      event.longitude = coordinates.longitude;
+
+    } catch {
+      console.log("Failed to get coordinates");
+      // Remove current values if new event location doesn't have coordinates
+      if (event.latitude) delete event.latitude;
+      if (event.longitude) delete event.longitude;
+
+    }
+    
   }
 
   try {
 
-    // If 
+    console.log(id);
 
     await db.collection("events").doc(id).set(event);
 
