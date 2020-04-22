aee09b414cf5b73325f4c22bfdee69a9b80fcbb2
Andrew Han <ajh5273@gmail.com>
2020-04-23 01:11:04 +1000

    Added a notification package and modified navigation to use it

--- SourceCode_and_Documentation/frontend/src/components/Dashboard/navigation.js
+++ SourceCode_and_Documentation/frontend/src/components/Dashboard/navigation.js
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import SignOutButton from './signout'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Toast } from 'react-bootstrap'
 import { withFirebase } from '../Firebase'
+import { BACKEND } from '../../constants/roles'
+import { AlertList, Alert, AlertContainer } from "react-bs-notifier";
 
 import '../../styles/responsive.css'
 import '../../styles/text.css'
 
 const Navigation = ({ firebase }) => {
+  const alerts = [{
+    id: 1,
+    type: "info",
+    message: "Hello, world"
+}, {
+    id: 2,
+    type: "success",
+    message: "Oh, hai"
+}]
+
+  const getNewNotifications = async () => {
+
+    const token = localStorage.getItem('token')
+    await axios({
+      url: BACKEND + '/user/new_notifications',
+      method: "GET",
+      headers: { 'Authorization': `${token}` },
+    }).then(response => {
+      console.log(response)
+    }).then(error => {
+      console.log(error)
+    })
+
+  }
+  useEffect(() => {
+    getNewNotifications()
+    const source = axios.CancelToken.source()
+    return () => {
+      source.cancel()
+    }
+  })
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     getNewNotifications()
+  //   }, 5000)
+
+  //   return () => clearInterval(interval)
+  // }, [])
 
   return (
     <div style={{ overflowX: 'show' }}>
-    
+<AlertList alerts={alerts} />
       {/* Coloured Bar */}
       <div className="row">
         <div className="col">
@@ -23,11 +64,12 @@ const Navigation = ({ firebase }) => {
       <div className="row">
         <div className="col">
           <Navbar collapseOnSelect expand="xl" variant="light" style={{ padding: '0 5vw 0 5vw' }}>
-          
+
             {/* Application Title */}
             <Navbar.Brand href="/dashboard/explore"> <div className="txt-title"> Holler <span className="dot"></span> </div> </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
             <Navbar.Collapse id="responsive-navbar-nav">
 
               {/* Left Part */}
@@ -45,16 +87,16 @@ const Navigation = ({ firebase }) => {
                   <Nav.Item> <Nav.Link className="txt-gradient d-nav-link" onClick={firebase.doSignOut}> Logout </Nav.Link> </Nav.Item>
                 </div>
               </Nav>
-              
-            
+
+
             </Navbar.Collapse>
           </Navbar>
         </div>
       </div>
-      
+
       <hr style={{ width: '80vw', color: 'black' }} />
     </div>
-)
+  )
 }
 
 
