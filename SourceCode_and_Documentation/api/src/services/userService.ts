f749ec293e8ef03b905922ab14101e0250cd3d52
The-TT-Hacker <timthacker97@gmail.com>
2020-04-23 00:52:45 +1000

    Added error handling for new notifications

--- SourceCode_and_Documentation/api/src/services/userService.ts
+++ SourceCode_and_Documentation/api/src/services/userService.ts
@@ -286,7 +286,7 @@ export async function getNewNotifications(uid: string): Promise<Notification[]> 
   
   } catch (e) {
     if (e.errorInfo) throw e.errorInfo.message;
-    else throw "Error";
+    else throw e;
   }
 }
 
